2b31d9e8c39015d5a1892111c6b0c9383b32a115
Yuvaraj <s.yuvaraj03@gmail.com>
2016-05-13 15:53:03 +0530

    closure

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ function functionName(){
 	console.log(2 + 3); 
 }
 functionName();
+
+//closure
+var a1 = 10;
+function a(){
+	var a2 = 20;
+	function inner(){
+		console.log("innner", a2);
+	}
+};
+a();
