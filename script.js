2009526d1f30a976e54f30bb1c63a6f4047d3b3a
Yuvaraj <s.yuvaraj03@gmail.com>
2016-05-19 21:37:52 +0530

    for loop

--- script.js
+++ script.js
@@ -145,4 +145,20 @@ catch (e){
 	console.log(e.name);
 	console.log(e.message);
 }
+//Loops
+
+function howMany(selectObject) {
+  var numberSelected = 0;
+  for (var i = 0; i < selectObject.options.length; i++) {
+    if (selectObject.options[i].selected) {
+      numberSelected++;
+    }
+  }
+  return numberSelected;
+}
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+  alert('Number of options selected: ' + howMany(document.selectForm.musicTypes))
+});
 
