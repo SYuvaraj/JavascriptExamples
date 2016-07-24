aec0f3e2b81c97dfde63e1c270ea962a2a5a8e32
Yuvaraj <s.yuvaraj03@gmail.com>
2016-07-24 23:09:20 +0530

    func exp

--- functions.js
+++ functions.js
@@ -9,3 +9,14 @@ console.log('before func call ==>', car.name);
 objname( car );
 console.log('after func call ==>', car.name);
 
+//function expression
+var square = function(number){
+	return number * number;
+}
+var x = square(2);
+console.log('func exp==>', x);
+
+var factorial = function fact(n){
+	return n<2 ? 1 : n*fact(n-1);
+}
+console.log('fact', factorial(3) );
